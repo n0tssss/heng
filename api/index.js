9dbc3d94b44115a172ca7f84cab3ccd91e1ef286
Nutssss <1656071287@qq.com>
2021-05-19 14:22:18 +0800

    就差美化了

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
-// 加载 file-system 文件系统
+// file-system 文件系统
 let fs = require("fs");
-// 加载 http 核心模块
+// http 核心模块
 let http = require("http");
-// 引入 Axios
+// Axios
 let axios = require("axios");
+// URL 模块
+let parseURL = require("url");
 
 // 读取配置文件
 let config = null;
@@ -11,8 +13,21 @@ fs.readFile("./config/lovexhj.json", (err, data) => {
     config = JSON.parse(data.toString());
 })
 
+// 返回类型
+function back(err, data) {
+    return JSON.stringify({
+        error: err,
+        data: data
+    });
+}
+
 // 创建 WEB 服务器
 http.createServer(function (request, response) {
+    // 跨域
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    response.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    response.setHeader("content-type", "application/json")
+
     // 获取请求地址
     let url = request.url;
 
@@ -22,13 +37,21 @@ http.createServer(function (request, response) {
     });
 
     // 获取记仇
-    if (url === "/get") {
-        axios.get(`https://gitee.com/api/v5/repos/${config.Gitee.owner}/${config.Gitee.repo}/issues?access_token=${config.Gitee.access_token}&state=open&sort=created&direction=desc&page=1&per_page=20`).then(res => {
+    if (url.includes("/get")) {
+        // 获取分页数据
+        let page = parseURL.parse(request.url, true);
+
+        // 数据验证
+        if (!page.query.page && !page.query.per_page) {
+            return response.end(back("分页参数缺失！", null));
+        }
+
+        axios.get(`https://gitee.com/api/v5/repos/${config.Gitee.owner}/${config.Gitee.repo}/issues?access_token=${config.Gitee.access_token}&state=open&sort=created&direction=desc&page=${page.query.page}&per_page=${page.query.per_page}`).then(res => {
             if (res.status == 200) {
-                return response.end(JSON.stringify(res.data));
+                return response.end(back(null, res.data));
             }
         }, err => {
-            return response.end(err);
+            return response.end(back(err, null));
         });
         return;
     }
@@ -41,11 +64,15 @@ http.createServer(function (request, response) {
             result += chunk;
         });
         request.on("end", function () {
-            result = JSON.parse(result);
+            try {
+                result = JSON.parse(result);
+            } catch {
+                return response.end(back("error", null));
+            }
 
             // 数据验证
-            if(result.password != config.Gitee.password) {
-                return response.end("error");
+            if (result.password != config.Gitee.password) {
+                return response.end(back("爬", null));
             }
             if (result.title != "" && result.body != "") {
                 axios.post(`https://gitee.com/api/v5/repos/${config.Gitee.owner}/issues`, {
@@ -56,15 +83,15 @@ http.createServer(function (request, response) {
                     body: result.body
                 }).then(res => {
                     if (res.status == 201) {
-                        return response.end(JSON.stringify("ok"));
+                        return response.end(back(null, "ok"));
                     }
                 }, err => {
-                    return response.end(JSON.stringify(err));
+                    return response.end(back(err, null));
                 });
             }
         });
     } else {
-        return response.end("404");
+        return response.end(back("404", null));
     }
 }).listen(3000, () => {
     console.log("服务端已启动！访问地址：http://localhost:3000");
--- js/lovexhj.js
+++ js/lovexhj.js
@@ -2,18 +2,22 @@ const lovexhj = new Vue({
     el: "#lovexhj",
     data() {
         return {
+            ServerBase: "http://localhost:3000", // Node 后端地址
             localConfig: null, // 本地配置
             jsonConfig: null, // Json 配置
+            wdnmdData: null, // 记仇数据
+            wdnmdIndex: 0, // 当前选择的记仇
+            wdnmdGo: true, // 是否在首页
+            title: "", // 记仇标题
+            body: "", // 记仇内容
         }
     },
-    created() {},
     mounted() {
         // 本地配置获取
         this.getLocalConfig();
         // Json 配置获取
         this.getJsonConfig();
     },
-    destroyed() {},
     methods: {
         // 本地配置获取
         getLocalConfig() {
@@ -42,12 +46,16 @@ const lovexhj = new Vue({
                 if (request.status == 200) {
                     this.jsonConfig = JSON.parse(request.responseText);
                     // 调试
-                    console.log(this.jsonConfig);
+                    // console.log(this.jsonConfig);
 
                     // 同步处理
                     setTimeout(() => {
                         // 书本大小设置
                         this.setBookSize();
+                        // 记仇获取
+                        this.getWdnmd();
+                        // 富文本编辑器创建
+                        this.createEditor();
                     }, 0);
                 }
             }
@@ -101,10 +109,142 @@ const lovexhj = new Vue({
             book.style.width = this.jsonConfig.lovexhj.lovexhjBookSize + "px";
             book.style.height = this.jsonConfig.lovexhj.lovexhjBookSize + 80 + "px";
         },
-        // 页面打开
-        pageOpen() {
-            let e = event.target;
-            e.classList.add("lovexhjBookActive");
+        // 封面打开
+        fmOpen() {
+            let fm = document.querySelector(".lovexhjBookFm");
+            if (fm.className.includes("lovexhjBookActive")) {
+                return fm.classList.remove("lovexhjBookActive");
+            }
+            fm.classList.add("lovexhjBookActive");
+        },
+        // 记仇获取
+        getWdnmd() {
+            axios.get(`${this.ServerBase}/get?page=${this.jsonConfig.lovexhj.pageloadNum[0]}&per_page=${this.jsonConfig.lovexhj.pageloadNum[1]}`).then(res => {
+                if (res.data.error) {
+                    return console.log(res.data.error);
+                }
+                if (res.data.data.length == 0) {
+                    return;
+                }
+
+                this.wdnmdData = res.data.data;
+
+                // 调试
+                // console.log(this.wdnmdData.data);
+
+                // 日期处理
+                for (let i = 0; i < this.wdnmdData.length; i++) {
+                    this.wdnmdData[i].created_at = new Date(this.wdnmdData[i].created_at).toLocaleDateString();
+                }
+            }, err => {
+                console.log(err);
+            });
+        },
+        // 记仇选择
+        selectWdnmd(i) {
+            this.wdnmdIndex = i;
+            this.ListOpen();
+        },
+        // 目录翻页
+        ListOpen() {
+            let list = document.querySelector(".lovexhjBookList");
+            if (list.className.includes("lovexhjBookActive")) {
+                list.classList.remove("lovexhjBookActive");
+                return document.querySelector(".lovexhjBookListBg").classList.remove("lovexhjBookActive", "lovexhjBookListBgActive");
+            }
+            list.classList.add("lovexhjBookActive");
+            document.querySelector(".lovexhjBookListBg").classList.add("lovexhjBookActive", "lovexhjBookListBgActive");
+        },
+        // 板块切换
+        move() {
+            let dom = document.querySelector(".lovexhjBookMove");
+            if (this.wdnmdGo) {
+                dom.style.transform = "translateX(-50%)";
+            } else {
+                dom.style.transform = "translateX(0%)";
+            }
+            this.wdnmdGo = !this.wdnmdGo;
+        },
+        // 富文本编辑器创建
+        createEditor() {
+            let E = window.wangEditor;
+            let ed = new E("#ed");
+            //  菜单配置
+            ed.config.menus = [
+                "head",
+                "bold",
+                // "fontSize",
+                // "fontName",
+                "italic",
+                "underline",
+                "strikeThrough",
+                // "indent",
+                // "lineHeight",
+                // "foreColor",
+                //" backColor",
+                "link",
+                "list",
+                // "todo",
+                "justify",
+                "quote",
+                // "emoticon",
+                "image",
+                "video",
+                "table",
+                // "code",
+                "splitLine",
+                "undo",
+                "redo",
+            ];
+            // 关闭粘贴样式
+            ed.config.pasteFilterStyle = false;
+            // 代码高亮
+            ed.highlight = this.hljs;
+            // 提示文本
+            ed.config.placeholder = "这个仇我先记上！";
+            // 配置 onchange 回调函数
+            ed.config.onchange = (content) => {
+                this.body = content;
+            }
+            // 高度设置
+            ed.config.height = 200;
+
+            // 创建
+            ed.create();
+
+            // 默认样式修改
+            document.querySelector(".w-e-toolbar").style.background = "transparent";
+            document.querySelector(".w-e-toolbar").style.borderWidth = "0";
+            document.querySelector(".w-e-text-container").style.background =
+                "transparent";
+            document.querySelector(".w-e-text-container").style.border =
+                "transparent";
+        },
+        // 记个仇
+        wdnmdSubmit() {
+            // 数据验证
+            if(!this.title && !this.body) {
+                return this.$message({
+                    message: "记仇也需要认真填写哦！",
+                    showClose: true,
+                    type: "warning"
+                });
+            }
+            let password = prompt("记仇也需要密码的：");
+            if(!password) {
+                return this.$message({
+                    message: "密码呢？",
+                    showClose: true,
+                    type: "warning"
+                });
+            }
+            axios.post(this.ServerBase + "/add", {
+                title: this.title,
+                body: this.body,
+                password
+            }).then(res => {
+                console.log(res);
+            }, err => {});
         },
     },
 })
